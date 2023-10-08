bc7906d8d87e649cad2800b7f67f52c121789b7a
Vivek Singh <petmongrels@gmail.com>
2023-10-08 18:34:35 +0530

    .

---
+++ browser-automation/src/SeleniumDriverFactory.js
@@ -0,0 +1,28 @@
+import _ from "lodash";
+import chrome from "selenium-webdriver/chrome";
+import {Builder} from "selenium-webdriver";
+
+let driver;
+
+class SeleniumDriverFactory {
+    static createDriver(binaryPath) {
+        if (!_.isNil(driver)) return driver;
+
+        const chromeOptions = new chrome.Options();
+        if (!_.isNil(binaryPath)) chromeOptions.setBinaryPath(binaryPath);
+
+        driver = new Builder().forBrowser('chrome')
+            .setChromeOptions(chromeOptions)
+            .build();
+        return driver;
+    }
+
+    static exitBrowser() {
+        if (_.isNil(driver)) return;
+
+        driver.quit();
+        driver = null;
+    }
+}
+
+export default SeleniumDriverFactory;
---
+++ browser-automation/src/SeleniumDriverFactory.test.js
@@ -0,0 +1,11 @@
+import SeleniumDriverFactory from "./SeleniumDriverFactory";
+
+test('should use chrome from path', async function () {
+    const driver = SeleniumDriverFactory.createDriver();
+    await driver.get('https://www.google.com').then(() => SeleniumDriverFactory.exitBrowser());
+});
+
+test('should use chrome from the provided path', async function () {
+    const driver = SeleniumDriverFactory.createDriver("~/.cache/selenium/chromedriver");
+    await driver.get('https://www.google.com').then(() => SeleniumDriverFactory.exitBrowser());
+});
