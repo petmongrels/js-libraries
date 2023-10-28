8a43ac80f3be026d04df6f66f2863fa3f99a8cc2
Vivek Singh <petmongrels@gmail.com>
2023-10-28 22:57:07 +0530

    .

--- browser-automation/src/SeleniumUtil.test.js
+++ browser-automation/src/SeleniumUtil.test.js
@@ -1,6 +1,7 @@
 import {SeleniumDriverFactory, SeleniumUtil} from "../index";
 
-test("attach", async () => {
+//this will work when chrome is launched with a new profile
+xtest("attach", async () => {
     const driver = SeleniumDriverFactory.createDriver({debugPort: 9227});
     await SeleniumUtil.switchToTab(driver, "Holdings / Console");
 });
